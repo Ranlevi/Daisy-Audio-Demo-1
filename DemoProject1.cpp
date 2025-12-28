7f25c8da8f72131de7be796c8f2657b6c3b7aaba
Ran Levi <ran.levi@gmail.com>
2025-12-28 08:43:14 +0200

    Version 0.2: Added a simple oscillator.

--- DemoProject1.cpp
+++ DemoProject1.cpp
@@ -1,23 +1,40 @@
 #include "daisy_seed.h" //High Level interface for the Daisy board for init/config.
+#include "daisysp.h"    //Digital Signal Processing library.
 
 /*
 Demo Project 1
 ---------------
 Author: Ran Levi
-Version: 0.1
+Version: 0.2
 
 The goal is to experiment with as many Daisy Pod features as possible.
 Version 0.1: Light LED_1 when pressing the SW1 button.
+Version 0.2: Add a simple, 1.5kHz oscillator to the phone jack output.
 */
 
 using namespace daisy;
 using namespace daisy::seed;
 
 DaisySeed hw; //A class inside namespace daisy.
+daisysp::Oscillator osc; 
+
+void Callback (AudioHandle::InputBuffer audio_in,
+			   AudioHandle::OutputBuffer audio_out,
+			   size_t size){
+	//Make sound
+	for (size_t i=0; i < size; i++){
+		float sine_signal = osc.Process();
+		audio_out[0][i] = sine_signal; //Left Channel
+		audio_out[1][i] = sine_signal; //Right Channel
+	}
+}
 
 int main(void)
 {
 	hw.Init(); //Init the Daisy Seed.
+
+	//-----------------------------------
+	//SW1 & LED_1
 
 	//An object for SW1 on the Pod.
 	//SW1 is connected to GPIO D27.
@@ -37,6 +54,15 @@ int main(void)
 	led1_R.Init(D20, GPIO::Mode::OUTPUT);
 	led1_G.Init(D19, GPIO::Mode::OUTPUT);
 	led1_B.Init(D18, GPIO::Mode::OUTPUT);
+
+	//---------------------------
+	//Audio generation.
+	osc.Init(hw.AudioSampleRate());
+	osc.SetFreq(1500.0);
+	osc.SetWaveform(daisysp::Oscillator::WAVE_SQUARE);
+	osc.SetAmp(0.1f);
+
+	hw.StartAudio(Callback);
 	
 	while(1) {
 		bool sw1_state = sw1.Read();
